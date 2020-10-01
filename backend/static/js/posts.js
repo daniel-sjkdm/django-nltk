7fb581adcea176def95a14ca4a28ad6f643029a5
daniel-sjkdm <daniel.py.216@gmail.com>
2020-10-01 00:56:18 -0500

    The modal is cleaned everytime a request is performed

--- backend/static/js/posts.js
+++ backend/static/js/posts.js
@@ -1,24 +1,27 @@
-console.log("Ready 4 now...")
-
 $(".detail-button").click((event) => {
     event.preventDefault();
     const id = event.target.id;
-    console.log("Post id = ", id);
     $.ajax({
         type: "GET",
-        url: `http://127.0.0.1:8000/api/posts/${id}`,
+        url: `http://127.0.0.1:8000/api/comments/${id}`,
         success: response => {
-            console.log(response);
-            console.log(response.comments)
+            $(".modal-body").empty();
+            if (response.length >= 1) {
+                response.map(comment => {
+                    $(".modal-body").append(`
+                        <div>
+                            <p> ${comment.content} </p>
+                            <hr/>
+                        </div>
+                    `);
+                });
+            } 
+            else {
+                $(".modal-body").html("The post has no comments!");
+            }
         },
         error: error => {
-            console.log(error)
+            console.log(error);
         }
     })
-    // $(".modal-title").html("Hello world");
-    // const comments = ["hello", "world"]
-    // $(".modal-body").html(`
-    //     ${comments.map(comment => `<section> ${comment} </section>`)}
-    //     <br/>
-    // `)
 });
